d5bf134217eed1a351d21431c777023856a8192d
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-06-20 15:11:11 +0800

    Field autowire for SequenceGeneratorService

--- src/main/java/com/pangan/studentregistration/sequence/SequenceGeneratorService.java
+++ src/main/java/com/pangan/studentregistration/sequence/SequenceGeneratorService.java
@@ -20,12 +20,8 @@ public class SequenceGeneratorService {
     public static final int AUTO_INCREMENT_BY_1 = 1;
     public static final long DEFAULT_ID = 1L;
 
-    private final MongoOperations mongoOperations;
-
     @Autowired
-    public SequenceGeneratorService(MongoOperations mongoOperations) {
-        this.mongoOperations = mongoOperations;
-    }
+    private MongoOperations mongoOperations;
 
     public long generateSequence(String sequenceId) {
         Query query = new Query(Criteria.where(ID).is(sequenceId));
